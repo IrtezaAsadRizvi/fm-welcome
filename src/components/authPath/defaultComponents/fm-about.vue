a3109992ca999347efe67654094b627cd4c56b70
IrtezaAsadRizvi <irteza.asad.cse@ulab.edu.bd>
2017-10-27 15:46:41 +0600

    started post page

--- src/components/authPath/defaultComponents/fm-about.vue
+++ src/components/authPath/defaultComponents/fm-about.vue
@@ -1,6 +1,68 @@
 <template>
-    <div>
-        fm about
+    <div id="fm-about">
+      <!-- the background -->
+      <div id="about-bg">
+        <span></span>
+        <span id="sec-bg"></span>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5" id="about-text">
+            <h1>FREEMIG</h1>
+            <h4>Let’s Learn! Go Earn! Boost your activities with fun!</h4>
+            <p>Freemig, all in a package is just in your fingertips. Avail opportunities as Freemig knows your ultimate desire.<br>
+               <a href="#">Sign up</a> and shape your world.
+            </p>
+            <a class="btn btn-default">Sign In</a>
+            <a class="btn btn-default">Sign Up</a>
+          </div>
+          <div class="col-md-5 col-md-offset-2 container">
+            <!-- about-grid -->
+            <div id="about-grid">
+              <div class="row" style="width:400px">
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_4.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_3.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_1.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_5.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_2.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_6.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_7.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_8.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_9.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_10.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_11.png">
+                </div>
+                <div class="col-md-4">
+                  <img src="../../../assets/images/about_icon_12.png">
+                </div>
+              </div>
+            </div>
+            <!-- end of about-grid -->
+          </div>
+        </div>
+      </div>
+      <!-- end of container -->
     </div>
 </template>
 
@@ -15,5 +77,4 @@
     }
 </script>
 <style>
-    /*@import "../../assets/style/authPath/default.css";*/
 </style>
--- src/components/authPath/defaultComponents/fm-post.vue
+++ src/components/authPath/defaultComponents/fm-post.vue
@@ -1,6 +1,25 @@
 <template>
-    <div>
-        fm post
+    <div id="fm-post">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5" id="post-text">
+            <h1>POST <span>& NEWSFEED</span></h1>
+            <p>
+              <span>Do we all have an idea how this works?</span>
+              A bucket with mood activities, a pile of interests,
+              and a switch to filter regular to professional life can
+              gives you a blow with excitement.
+            </p>
+            <div id="post-img-1">
+              <img src="../../../assets/images/fm-post-img2.png">
+            </div>
+          </div>
+          <div class="col-md-5 col-md-offset-2 container">
+
+          </div>
+        </div>
+      </div>
+      <!-- end of container -->
     </div>
 </template>
 
@@ -12,5 +31,5 @@
     }
 </script>
 <style>
-    /*@import "../../assets/style/authPath/default.css";*/
+
 </style>
